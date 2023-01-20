af53aff4a5aa5f8f2d854d3e19f466b4d7789759
Alessandro Privitera <alfiomuschio@gmail.com>
2023-01-20 13:17:33 +0100

    Update Jan 20, 2023

--- Assets/SDNLib/Lib/WallFiltAndGain.cs
+++ Assets/SDNLib/Lib/WallFiltAndGain.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class WallFiltAndGain : MonoBehaviour {
     // A simple container for wall properties chosen by the designer
 
-    public float wall_absorption_coeff = 0.95f;
-    public int wall_index = 0;
-    public string wall_material = "concrete";
+    public float wall_absorption_coeff;
+    public int wall_index;
+    public string wall_material;
 
 }
